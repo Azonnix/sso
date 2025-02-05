a2b7b815f284db64ae520dba4292b675cdcc721a
a.alekseev <alekseeval.1996@gmail.com>
2025-02-05 23:31:54 +0300

    first commit

---
+++ cmd/sso/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/azonnix/sso/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case envLocal:
+		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return logger
+}
