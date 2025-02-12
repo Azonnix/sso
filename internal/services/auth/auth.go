8bda9eec915499bf70d0e6b00ef39bba7f645960
a.alekseev <alekseeval.1996@gmail.com>
2025-02-12 23:12:54 +0300

    start implement auth service

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -1,1 +1,50 @@
 package auth
+
+import (
+	"context"
+	"github.com/azonnix/sso/internal/domain/models"
+	"log/slog"
+	"time"
+)
+
+type UserSaver interface {
+	SaveUser(ctx context.Context, email string, passHash []byte) (int64, error)
+}
+
+type UserProvider interface {
+	GetUser(ctx context.Context, email string) (models.User, error)
+	IsAdmin(ctx context.Context, userId int64) (bool, error)
+}
+
+type AppProvider interface {
+	GetApp(ctx context.Context, appId int64) (models.App, error)
+}
+
+type Auth struct {
+	log          *slog.Logger
+	userSaver    UserSaver
+	userProvider UserProvider
+	appProvider  AppProvider
+	tokenTTL     time.Duration
+}
+
+// New return a new instance of Auth service
+func New(
+	log *slog.Logger,
+	userSaver UserSaver,
+	userProvider UserProvider,
+	appProvider AppProvider,
+	tokenTTL time.Duration,
+) *Auth {
+	return &Auth{
+		log:          log,
+		userSaver:    userSaver,
+		userProvider: userProvider,
+		appProvider:  appProvider,
+		tokenTTL:     tokenTTL,
+	}
+}
+
+func (a *Auth) Login(ctx context.Context, email, password string, userId int64) (models.User, error) {
+	return models.User{}, nil
+}
